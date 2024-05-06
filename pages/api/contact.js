f734826bc279c38edc83fda3f79a9e22b93452d9
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-05-06 17:03:45 +0300

    add

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -24,7 +24,7 @@ async function sendProperty({ name, email, phone, desc ,property}) {
   const emailOptions = {
     form: email,
     //halilazyz@gmail.com
-    to:["halilazyz@gmail.com","Muhammedhusyin039@gmail.com"],
+    to:["halilazyz@gmail.com"],
     subject: `Contact Message from ${email}`,
     html: `<body style="background: #f3f4f6; padding:0 5px;">
     <div style="max-width: 600px; margin: 0 auto; padding: 20px 0; font-family:Trebuchet MS;">
@@ -63,7 +63,7 @@ async function sendContact({ name, email, phone, desc}) {
 const emailOptions = {
   form: email,
   //halilazyz@gmail.com
-  to:["halilazyz@gmail.com" ,"Muhammedhusyin039@gmail.com"], 
+  to:["halilazyz@gmail.com" ], 
   //"basma94ghanem@gmail.com",
   subject: `Contact Message from ${email}`,
   html: `<body style="background: #f3f4f6; padding:0 5px;">
